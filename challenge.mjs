0330c30a728c394b054795c7232c3aa76e354b8e
salehinafnan <salehinafnan@gmail.com>
2024-01-14 18:25:50 +0600

    Refactor blockingGet and push functions

--- challenge.mjs
+++ challenge.mjs
@@ -1,3 +1,25 @@
-export async function blockingGet(key) {}
+// JavaScript
+let dataQueue = {};
 
-export async function push(key, data) {}
+export async function blockingGet(key) {
+  return new Promise((resolve, reject) => {
+    let checkData = setInterval(() => {
+      if (dataQueue[key] && dataQueue[key].length > 0) {
+        resolve(dataQueue[key].shift());
+        clearInterval(checkData);
+      }
+    }, 100);
+
+    setTimeout(() => {
+      clearInterval(checkData);
+      resolve(null);
+    }, 30000);
+  });
+}
+
+export async function push(key, data) {
+  if (!dataQueue[key]) {
+    dataQueue[key] = [];
+  }
+  dataQueue[key].push(data);
+}
